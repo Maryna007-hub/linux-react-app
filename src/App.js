a3bf0c4defd30a011daaa998271a2cc52fd70b25
Maryna007-hub <126612246+Maryna007-hub@users.noreply.github.com>
2023-09-07 16:56:51 +0100

    upd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
 
     <div className="mx-auto max-w-screen-md mt-4 py-32 px-32 bg-gradient-to-br 
-    from-cyan-600 to-blue-700 h-fit shadow-xl shadow-gray-400 rounded-[5px]">
+    from-cyan-600 to-blue-700 h-fit shadow-xl shadow-gray-400 rounded-[7px]">
       <TopButtons/>
     </div>
   );
