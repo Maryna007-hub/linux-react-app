d39d815e7288542ad0f151d4f321906b798fe1e0
Maryna007-hub <126612246+Maryna007-hub@users.noreply.github.com>
2023-09-07 22:56:06 +0100

    sum

--- src/components/TimeAndLocation.js
+++ src/components/TimeAndLocation.js
@@ -8,7 +8,10 @@ function TimeAndLocation() {
               Monday, 7 September 2023   |   Local time: 12:55 PM
             </p>
         </div>
-        
+        <div className='flex items-center justify-center my-3'>
+         <p className='text-white text-3xl font-medium'> Lisbon, PT 
+              </p>
+        </div>
     </div>
   )
 }
