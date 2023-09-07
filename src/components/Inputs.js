697b3a676425091a3490b140ffa35ded69739040
Maryna007-hub <126612246+Maryna007-hub@users.noreply.github.com>
2023-09-07 18:38:32 +0100

    upd

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons';
+
 function Inputs() {
   return (
-    <div> className='flex flex-row justify-center my-6'
+    <div className='flex flex-row justify-center my-6'>
       <div className='flex flex-row w-3/4 items-center justify-center space-x-4'>
         <input type='text'
          placeholder='Search for a city...'
@@ -22,6 +23,7 @@ function Inputs() {
            °F</button>
       </div>
     </div>
+    
   )
 }
 
