849d431f8285638be5a37c453415c9e80d1c37e1
Maryna007-hub <126612246+Maryna007-hub@users.noreply.github.com>
2023-09-07 17:15:04 +0100

    sum

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import TopButtons from './components/TopButtons';
+import Inputs from './components/Inputs';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br 
     from-cyan-600 to-blue-700 h-fit shadow-xl shadow-gray-400 rounded-[7px]">
       <TopButtons/>
+      <Inputs/>
     </div>
   );
 }
