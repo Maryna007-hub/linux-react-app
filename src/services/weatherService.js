c92e228ae5fcc37cc9dde604b5e13384b9375a4d
Maryna007-hub <126612246+Maryna007-hub@users.noreply.github.com>
2023-09-08 15:50:11 +0100

    upd

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -6,13 +6,12 @@ const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
 
 // https://api.openweathermap.org/data/3.0/onecall?
 //lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
 const getWeatherData = (infoType,searchParams) => {
     const url = new URL(BASE_URL + '/' + infoType);
     url.search = new URLSearchParams({...searchParams, appid:API_KEY});
-
    
-    return fetch(url).then((res) => res.json())
-    
+    return fetch(url).then((res) => res.json())  
 }
 
 const formatCurrentWeather = (data) => {
